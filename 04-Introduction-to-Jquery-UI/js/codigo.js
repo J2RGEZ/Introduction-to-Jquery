651d4c78397d012d7568b54685e5572390cd139a
J2RGEZ <j2rgez@gmail.com>
2016-10-24 08:56:15 +0100

    Add task manager.

--- 04-Introduction-to-Jquery-UI/js/codigo.js
+++ 04-Introduction-to-Jquery-UI/js/codigo.js
@@ -1,6 +1,30 @@
+var agregarFecha = null;
 //Dragging elements with Jquery UI
 
 $(".columnas").sortable({
   connectWith: ".bloque",
   handle: ".titulo"
+});
+
+//Add task form
+$("#agregarTarea").click(function(event) {
+  $("header form#agregar").slideToggle("fast");
+  $("header form#buscarTarea").slideToggle("fast");
+});
+
+//Calendar
+$("#fecha1").datepicker({
+  changeMonth=true,
+  changeYear=true
+});
+
+$("#fecha2").datepicker({
+  changeMonth=true,
+  changeYear=true
+});
+
+//Save task
+$("#guardar").click(function(event) {
+  agregarFecha = $("#fecha1").val();
+  $(.columnas).append('Some text')
 });
