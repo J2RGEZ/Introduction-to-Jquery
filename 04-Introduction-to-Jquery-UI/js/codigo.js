28df7a2f6d79d198ab9dbb388a132412fc397e11
j2rgez <j2rgez@gmail.com>
2016-10-24 16:45:43 +0100

    Finish Jquery UI and add more features to the task manager.

--- 04-Introduction-to-Jquery-UI/js/codigo.js
+++ 04-Introduction-to-Jquery-UI/js/codigo.js
@@ -1,30 +1,61 @@
-var agregarFecha = null;
+var agregarFecha;
+var buscarFecha = null;
+var tareas = localStorage.getItem("tarea");
+
+$(".columnas").html(tareas);
 //Dragging elements with Jquery UI
 
 $(".columnas").sortable({
   connectWith: ".bloque",
   handle: ".titulo"
-});
+})
 
 //Add task form
-$("#agregarTarea").click(function(event) {
+$("#agregarTarea").click(function() {
   $("header form#agregar").slideToggle("fast");
   $("header form#buscarTarea").slideToggle("fast");
-});
+})
 
 //Calendar
 $("#fecha1").datepicker({
-  changeMonth=true,
-  changeYear=true
-});
+  changeMonth:true,
+  changeYear:true
+})
 
 $("#fecha2").datepicker({
-  changeMonth=true,
-  changeYear=true
-});
+  changeMonth:true,
+  changeYear:true
+})
 
 //Save task
-$("#guardar").click(function(event) {
-  agregarFecha = $("#fecha1").val();
-  $(.columnas).append('Some text')
-});
+$("#guardar").click(function() {
+  if($("#fecha1").val() != ""){
+    agregarFecha = $("#fecha1").val();
+    $(".columnas").append('<li class="bloque"><div class="titulo">'+$("#tituloTarea").val()+'<span class="eliminar">X</span></div><div class="fecha">'+agregarFecha+'</div><div class="contenido">'+$("#descripcionTarea").val()+'</div></li>');
+    localStorage.setItem("tarea",$(".columnas").html());
+  }else{
+    $("#dialog").dialog();
+  }
+})
+
+//Delete task
+$(".eliminar").click(function() {
+  $(this).parent().parent().remove();
+  localStorage.setItem("tarea",$(".columnas").html());
+})
+
+//Search task
+$("#buscar").click(function() {
+  if($("#fecha2").val() != ""){
+    $(".bloque").hide();
+    buscarFecha = $("#fecha2").val();
+    var fechas = $(".fecha");
+    for(var i = 0; i < fechas.length; i ++){
+      if($(fechas[i]).html() == buscarFecha){
+        $(fechas[i]).parent().show();
+      }
+    }
+  }else {
+    $("#dialog").dialog();
+  }
+})
